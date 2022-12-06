c17f24e1255287cdc214a64382a1f0ebe3c48693
Junya Okabe <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-12-07 04:09:02 +0900

    add: .sgitファイルの初期化用関数

---
+++ pkg/init/init_test.go
@@ -0,0 +1,37 @@
+package init
+
+import (
+	"os"
+	"testing"
+)
+
+func TestMain(m *testing.M) {
+	// Call the function we are testing
+	initializeDir()
+
+	// Run the tests
+	m.Run()
+
+	// Remove the .sgit directory
+	removeDir()
+}
+
+func TestInitializeDir(t *testing.T) {
+	// Check if the .sgit/objects directory exists
+	_, err := os.Stat(".sgit/objects")
+	if err != nil {
+		t.Error("Error: ", err)
+	}
+
+	// Check if the .sgit/objects/info directory exists
+	_, err = os.Stat(".sgit/objects/info")
+	if err != nil {
+		t.Error("Error: ", err)
+	}
+
+	// Check if the .sgit/objects/pack directory exists
+	_, err = os.Stat(".sgit/objects/pack")
+	if err != nil {
+		t.Error("Error: ", err)
+	}
+}
