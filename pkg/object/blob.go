666f6e7f8f92452076582628ea2d3b26919c4010
Junya Okabe <okabe.junya.qj@alumni.tsukuba.ac.jp>
2022-12-14 03:36:56 +0900

    bugfix: 文字化け周りのバグを修正

--- pkg/object/blob.go
+++ pkg/object/blob.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"compress/zlib"
 	"crypto/sha1"
+	"encoding/hex"
 	"fmt"
 	"io"
 	"os"
@@ -24,22 +25,26 @@ func compress(r io.Reader) (io.Reader, error) {
 }
 
 func StoreBlob(content string) {
-	// BUGFIX: なんかバグってるので直せ
 	header := "blob " + strconv.Itoa(len(content)) + "\x00"
 
+	// sha1 ハッシュを計算する
 	store := header + content
-	sha1_store := sha1.Sum([]byte(store))
+	h := sha1.New()
+	h.Write([]byte(store))
+	hs := h.Sum(nil)
+	sha1_store := hex.EncodeToString(hs)
 
+	// zlib でファイルを圧縮する
 	zr, err := compress(bytes.NewReader([]byte(store)))
 	if err != nil {
 		fmt.Println("Error: ", err)
 	}
 	b := zr.(*bytes.Buffer)
 
+	// ファイルを作成する
 	dir_path := sha1_store[0:2]
 	file_path := sha1_store[2:]
 	dir.CreateDir(string(dir_path))
-	dir.CreateFile(string(file_path))
 	full_path := string(dir_path) + "/" + string(file_path)
 
 	f, err := os.Create(full_path)
--- pkg/object/blob_test.go
+++ pkg/object/blob_test.go
@@ -2,7 +2,15 @@ package object
 
 import "testing"
 
+func TestMain(m *testing.M) {
+	// Setup
+	m.Run()
+	// Teardown
+	// TODO: Remove test directory
+}
+
 func TestStoreBlob(t *testing.T) {
 	content := "hello world"
 	StoreBlob(content)
+
 }
